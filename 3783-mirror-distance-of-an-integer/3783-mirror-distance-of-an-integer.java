61e0bb17fe6b0605d6367796547892d408a7c5a8
Dev Anandhan M <da89fhaj@gmail.com>
2026-04-18 18:35:11 +0530

    Time: 2 ms (15.53%), Space: 42.9 MB (15.54%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
@@ -1,12 +1,15 @@
-// Added using AI
 class Solution {
-    private int revnum(int a) {
-        if (a / 10 == 0) return a ;
-        String s = new StringBuilder(Integer.toString(a)).reverse().toString();
-        return Integer.parseInt(s) ;
-    }
+     private int revnum(int a){
+        if(a/10==0) return a;
+            String s = new StringBuilder(Integer.toString(a)).reverse().toString();
+             return Integer.parseInt(s);
+     }
 
-    public int mirrorDistance(int n) {
-        return Math.abs(n - revnum(n)) ;
-    }
-}
+      public int mirrorDistance(int n){
+          return Math.abs(n - revnum(n));
+      }
+  }
+          
+        
+
+
