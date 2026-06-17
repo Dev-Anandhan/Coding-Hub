411c076d7c707509440555d892c08229adb6114d
Dev Anandhan M <da89fhaj@gmail.com>
2026-06-17 10:55:37 +0530

    Time: 1 ms (99.97%), Space: 46 MB (96.03%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] seenRows = new boolean[9][9];
+        boolean[][] seenCols = new boolean[9][9];
+        boolean[][] seenBoxes = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+
+                int num = c - '1';              // 0 to 8
+                int boxIndex = (i / 3) * 3 + j / 3;
+
+                if (seenRows[i][num] || seenCols[j][num] || seenBoxes[boxIndex][num]) {
+                    return false;
+                }
+
+                seenRows[i][num] = true;
+                seenCols[j][num] = true;
+                seenBoxes[boxIndex][num] = true;
+            }
+        }
+
+        return true;
+    }
+}
